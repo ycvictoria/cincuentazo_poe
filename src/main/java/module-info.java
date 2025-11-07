1cc4e2100af65da4cec2562bf2d7dcf2c60e80d2
Juan David Sanchez Navarro <juan.sanchez.navarro@correounivalle.edu.co>
2025-11-07 18:12:38 -0500

    .

--- src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module org.example.miniproyecto3 {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens org.example.miniproyecto3 to javafx.fxml;
-    exports org.example.miniproyecto3;
-}
--- src/main/java/org/example/miniproyecto3/Launcher.java
+++
@@ -1,9 +0,0 @@
-package org.example.miniproyecto3;
-
-import javafx.application.Application;
-
-public class Launcher {
-    public static void main(String[] args) {
-        Application.launch(HelloApplication.class, args);
-    }
-}
